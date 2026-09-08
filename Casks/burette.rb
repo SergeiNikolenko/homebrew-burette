8228ad3da8e047c34b32c24e8949b8558885678d
Sergei A. Nikolenko <Nikolenko.Sergei@icloud.com>
2026-09-08 19:21:28 +0300

    Update Burette to 2.3.16

--- Casks/burette.rb
+++ Casks/burette.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "burette" do
-  version "2.3.11"
-  sha256 "1a9ccbd77c572fb4e56fc172453da87ef152c7b90f6838700d2c5cb45d3b16dd"
+  version "2.3.16"
+  sha256 "b2b7c5a46e3b1a13f613d90a4ab26ff12c5f4348a1dacdd6e4cfe3518ef5427d"
 
   url "https://github.com/SergeiNikolenko/Burette/releases/download/v#{version}/Burette-#{version}.zip"
   name "Burette"
